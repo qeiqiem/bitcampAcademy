88ad1b905244ef92400983397657968e36103cb4
taeyeon Hwang <71449312+hty019@users.noreply.github.com>
2021-01-30 04:08:45 +0900

    Update BoardListController.java

--- src/spms/controls/BoardListController.java
+++ src/spms/controls/BoardListController.java
@@ -1,14 +1,11 @@
 package spms.controls;
 
 import java.util.Map;
-import java.util.Set;
-
 import spms.annotation.Component;
-import spms.bind.DataBinding;
 import spms.dao.BoardDao;
 import spms.vo.Post;
 @Component("/board/boardlist.do")
-public class BoardListController implements Controller,DataBinding {
+public class BoardListController implements Controller {
 
 	BoardDao boardDao = null;
 	
@@ -19,19 +16,8 @@ public class BoardListController implements Controller,DataBinding {
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
 		Post post = (Post)model.get("post");
-		if(post.getSearch()==null) {//검색어가 없다면
 			model.put("posts", boardDao.selectList());
 			model.put("page", boardDao.initPage());
 			return "/board/BoardList.jsp";
-		} else {//검색어가 있다면(or 아무것도 입력하지 않고 검색버튼을 눌렀을 경우)
-			model.put("posts", boardDao.searchedList(post));
-			return "/board/BoardList.jsp";
-		}
-	}
-	@Override
-	public Object[] getDataBinders() {
-		return new Object[] {
-			"post", spms.vo.Post.class
-		};
 	}
 }
