76e315b0a2362007430ddcd9dadca6e2d958e651
taeyeon Hwang <71449312+hty019@users.noreply.github.com>
2021-01-30 04:09:07 +0900

    Update ProjectDao.java

--- src/spms/dao/ProjectDao.java
+++ src/spms/dao/ProjectDao.java
@@ -2,13 +2,15 @@ package spms.dao;
 
 import java.util.List;
 
+import spms.vo.Page;
 import spms.vo.Post;
 
 public interface ProjectDao {
 	List<Post> selectList() throws Exception;
-	List<Post> searchedList(String title) throws Exception;
-	int insert(Post board) throws Exception;
+	List<Post> searchedList(Post post, Page page) throws Exception;
+	Page searchedPage(Page page) throws Exception;
+	int insert(Post post) throws Exception;
 	Post selectOne(int no) throws Exception;
-	int update(Post board) throws Exception;
+	int update(Post post) throws Exception;
 	int delete(int no) throws Exception;
 }
