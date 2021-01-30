f48793bb7d7b247e8bc520391f911a8df59d9ad3
taeyeon Hwang <71449312+hty019@users.noreply.github.com>
2021-01-30 18:49:51 +0900

    Update PageController.java

--- src/spms/controls/PageController.java
+++ src/spms/controls/PageController.java
@@ -21,8 +21,8 @@ public class PageController implements Controller, DataBinding {
 	public String execute(Map<String, Object> model) throws Exception {
 		Post post = (Post)model.get("post");
 		Page page = (Page)model.get("page");
-			model.put("posts", boardDao.searchedList(post, page));
-			model.put("page",boardDao.searchedPage(page));
+		model.put("page",boardDao.searchedPage(page));
+		model.put("posts", boardDao.searchedList(post, page));
 		return "/board/BoardList.jsp";	
 	}
 	@Override
