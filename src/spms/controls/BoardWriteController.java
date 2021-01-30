1bfdc76624bf59d8006851331b98ae1f42fc9d66
Ka Kim <76952809+qeiqiem@users.noreply.github.com>
2021-01-30 23:13:04 +0900

    Add files via upload

---
+++ src/spms/controls/BoardWriteController.java
@@ -0,0 +1,42 @@
+package spms.controls;
+
+import java.util.Map;
+
+import spms.annotation.Component;
+import spms.bind.DataBinding;
+import spms.dao.BoardDao;
+import spms.vo.Post;
+
+@Component("/board/write.do")
+public class BoardWriteController implements Controller,DataBinding {
+
+	BoardDao boardDao = null;
+	
+	public BoardWriteController setBoardDao(BoardDao boardDao) {
+		this.boardDao = boardDao;
+		return this;
+	}
+
+	
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		Post post = (Post)model.get("post");
+		if(post!=null) {
+			model.put("posts", boardDao.insert(post));
+			return "/board/write.jsp";
+		}
+		else {
+			return "/board/boardlist.do";
+//			return "redirect:/board/boardlist.do\";
+		}
+		
+	}
+	
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+			"post", spms.vo.Post.class
+		};
+	}
+
+}
---
+++ src/spms/controls/LogController.java
@@ -0,0 +1,13 @@
+package spms.controls;
+
+import java.util.Map;
+
+public class LogController implements Controller {
+
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
