1278647e2e2c08ef1ebd1ef258d182a44fdb703e
jangPaul-Dev <fnzlvj365@gmail.com>
2021-03-21 20:37:40 +0900

    이상한 주석 제거

--- kkaekkt/src/main/webapp/js/map.js
+++ kkaekkt/src/main/webapp/js/map.js
@@ -60,7 +60,5 @@ $(document).ready(function(){
       function viewSearch(clone){
          $(".slide_mini").html(clone+"&nbsp&nbsp검색결과")  
       }
-=======
->>>>>>> b26679f544bcfaf13c729914a757e8124b0f5ff1
 
 })
