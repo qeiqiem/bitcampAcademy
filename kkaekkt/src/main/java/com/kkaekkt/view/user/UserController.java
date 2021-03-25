8ce5c033bb3fa36ba91483ce1343ebe0926f8d51
kimso <122kim122so@gmail.com>
2021-03-25 17:15:35 +0900

    일반유저, 소셜유저 디비에 정보 없을 시 회원가입창으로 돌림 업체유저 하는 중

--- kkaekkt/src/main/java/com/kkaekkt/view/user/UserController.java
+++ kkaekkt/src/main/java/com/kkaekkt/view/user/UserController.java
@@ -26,7 +26,7 @@ import com.kkaekkt.biz.user.UserService;
 public class UserController {
 	@Autowired
 	UserService userService;
-	
+
 //	@RequestMapping(value="/login.do", method=RequestMethod.POST) //인터페이스로 VO를 합칠지 고민 중..
 //	public String Join(PersonVO vo) {
 //		userService.insertUser(vo);		
@@ -37,24 +37,26 @@ public class UserController {
 //		userService.insertUser(vo);		
 //		return "Join.html";
 //	}
-	@RequestMapping(value="/likeOff.do",method=RequestMethod.POST)
+	@RequestMapping(value = "/likeOff.do", method = RequestMethod.POST)
 	@ResponseBody
 	public void likeOff(BusinessVO vo) {
 		userService.likeOff(vo);
 	}
-	@RequestMapping(value="/getLikedBs.do",method=RequestMethod.POST,produces="application/text;charset=utf-8")
+
+	@RequestMapping(value = "/getLikedBs.do", method = RequestMethod.POST, produces = "application/text;charset=utf-8")
 	@ResponseBody
 	public String getLikedBs(BusinessListVO vo) {
-		Gson gson=new Gson();
+		Gson gson = new Gson();
 		return gson.toJson(userService.getLikedBs(vo));
 	}
+
 	// 아이디 중복체크
-	@RequestMapping(value ="/idchk.do", method = RequestMethod.POST)
+	@RequestMapping(value = "/idchk.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String idchk(PersonVO vo) {
 		System.out.println(vo);
 		Gson gson = new Gson();
-		 vo.setState(userService.idchk(vo));
+		vo.setState(userService.idchk(vo));
 		return gson.toJson(vo);
 	}
 
@@ -81,136 +83,154 @@ public class UserController {
 // 		userService.insertUser(vo);
 // 		return "index.jsp";
 // 	}
-	
+
 	// 회원가입-업체
-	@RequestMapping(value="/joinBs.do", method=RequestMethod.POST)
+	@RequestMapping(value = "/joinBs.do", method = RequestMethod.POST)
 	public String Join(BusinessVO vo) {
 		System.out.println("메서드 진입");
 		userService.insertUser(vo);
 		return "index.jsp";
 	}
-	@RequestMapping(value="/updatePs.do", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/updatePs.do", method = RequestMethod.POST)
 	public String Update(PersonVO vo, HttpSession session) {
 		System.out.println(vo);
 		userService.updateUser(vo);
-	
+
 		PersonVO person = userService.getUser(vo);
 		session.setAttribute("person", person);
-		
+
 		return "/jsp/mypageUser/mybio.jsp";
 	}
-	
+
 	// 일반유저 로그인
-		@RequestMapping(value = "/loginPs.do", method = RequestMethod.POST)
-		public String Login(PersonVO vo, HttpSession session) {
+	@RequestMapping(value = "/loginPs.do", method = RequestMethod.POST)
+		public String Login(PersonVO vo, HttpSession session) throws Exception{
+			try {
+				// 로그인 성공
 			System.out.println("로그인처리");
 
-			vo = userService.getUser(vo);
+			//vo = userService.getUser(vo);
+			PersonVO user = userService.getUser(vo);
 
+			
 			System.out.println(vo); // 뭐가 담기는 지 보려했다
 
-			if (vo.getMno() == 0) {
+			if (user.getMno() == 0) {
 				System.out.println("회원정보없음");
-				session.setAttribute("person", null);
-				return "/jsp/login/loginPs.jsp";
-			} else {
+				//session.setAttribute("person", null);
+				return "/jsp/loginPs.jsp";
+			} else if(user.getMno() != 0) {
 				session.setAttribute("person", vo);
 			}
 			return "/jsp/indexPerson.jsp";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("로그인 실패");
+			return "/jsp/join/joinPerson.jsp";
 		}
+}
+	// 업체유저 로그인
+	@RequestMapping(value = "/loginBs.do", method = RequestMethod.POST)
+	public String Login(BusinessVO vo, HttpSession session) {
+		System.out.println("로그인처리");
 
-		// 업체유저 로그인
-		@RequestMapping(value = "/loginBs.do", method = RequestMethod.POST)
-		public String Login(BusinessVO vo, HttpSession session) {
-			System.out.println("로그인처리");
+		vo = userService.getUser(vo);
 
-			vo = userService.getUser(vo);
+		System.out.println(vo); // 뭐가 담기는 지 보려했다
 
-			System.out.println(vo); // 뭐가 담기는 지 보려했다
-
-			if (vo.getMno() == 0) {
-				System.out.println("회원정보없음");
-				session.setAttribute("personBs", null);
-				return "/jsp/login/loginBs.jsp";
-			} else {
-				session.setAttribute("personBs", vo);
-			}
+		if (vo.getMno() == 0) {
+			System.out.println("회원정보없음");
+			session.setAttribute("personBs", null);
+			return "/jsp/login/loginBs.jsp";
+		} else {
+			session.setAttribute("personBs", vo);
 			return "/jsp/indexCompany.jsp";
 		}
+	}
 
-		// 소셜유저 로그인	
-		@PostMapping(value = "/loginSNS.do")
-		public @ResponseBody String method(HttpServletRequest req, HttpServletResponse resp, PersonVO vo, HttpSession session) {
-			System.out.println("소셜유저 로그인처리");
-			//vo = userService.method(vo, req);
-			
-			System.out.println(vo); // 뭐가 담기는 지 보려했다		
-			
-			if (vo.getMno() == 0) {
-				System.out.println("회원정보없음");
-				session.setAttribute("person", null);
-				return "/jsp/login/loginPs.jsp";
-			} else {
-				System.out.println("회원정보있음~");
-				session.setAttribute("person", vo);
+	// 소셜로그인
+	@RequestMapping(value = "/loginSNS.do", method = RequestMethod.POST)
+	public @ResponseBody String kakaologin(PersonVO vo, HttpSession session, HttpServletResponse response)
+			throws Exception {
+		System.out.println("카카오 로그인 컨트롤러 접속");
+		try {
+			// 로그인 성공했을 때
+			vo = userService.method(vo);
+
+			PersonVO user = vo;
+			System.out.println(vo + "vo카카오"); // 카카오 로그인시 vo 확인
+			System.out.println("user카카2" + user);
+
+			if (user.getMno() != 0) {
+				session.setAttribute("person", user);
+				System.out.println("user정보 " + user);
+				System.out.println("vo정보" + vo);
+				return "/jsp/indexPerson.jsp";
+			} else if (user.getMno() == 0) {
+				session.setAttribute("person", user);
+				System.out.println("user없는거 출력" + user);
+				return "/jsp/join/joinPerson.jsp";
 			}
 
-//			PrintWriter pw;
-//			try {
-//				pw = resp.getWriter();
-//				pw.println("/jsp/indexPerson.jsp");
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-			System.out.println("return /jsp/indexPerson.jsp");
 			return "/jsp/indexPerson.jsp";
-		}
 
-		// 로그아웃
-		@RequestMapping("/logout.do")
-		public String logout(HttpSession session) {
-			System.out.println("로그아웃 처리");
-			session.invalidate();
-			return "/jsp/index.jsp";
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("카카오 로그인 실패");
+			return "/jsp/join/joinNoPs.jsp";
 		}
+	}
+
+	// 로그아웃
+	@RequestMapping("/logout.do")
+	public String logout(HttpSession session) {
+		System.out.println("로그아웃 처리");
+		session.invalidate();
+		return "/jsp/index.jsp";
+	}
+
 	// 일반사양관리
-		@RequestMapping(value="/selectComspec.do", method=RequestMethod.POST, produces="application/text;charset=utf-8")
-		@ResponseBody
-		public String SelcetComspec(BusinessVO vo) {
-			System.out.println(vo);
-			Gson gson=new Gson();
-			String comspec=gson.toJson(userService.getComspec(vo));
-			//System.out.println("test:" + comspec);
-			return comspec;
-		}
+	@RequestMapping(value = "/selectComspec.do", method = RequestMethod.POST, produces = "application/text;charset=utf-8")
+	@ResponseBody
+	public String SelcetComspec(BusinessVO vo) {
+		System.out.println(vo);
+		Gson gson = new Gson();
+		String comspec = gson.toJson(userService.getComspec(vo));
+		// System.out.println("test:" + comspec);
+		return comspec;
+	}
+
 	// 일반설비관리
-		@RequestMapping(value="/selectCoinspec.do", method=RequestMethod.POST, produces="application/text;charset=utf-8")
-		@ResponseBody
-		public String SelcetCoinspec(BusinessVO vo) {
-			System.out.println(vo);
-			Gson gson=new Gson();
-			String coinspec=gson.toJson(userService.getCoinspec(vo));
-			System.out.println("test:" + coinspec);
-			
-			return coinspec;			
-			
-		}	
+	@RequestMapping(value = "/selectCoinspec.do", method = RequestMethod.POST, produces = "application/text;charset=utf-8")
+	@ResponseBody
+	public String SelcetCoinspec(BusinessVO vo) {
+		System.out.println(vo);
+		Gson gson = new Gson();
+		String coinspec = gson.toJson(userService.getCoinspec(vo));
+		System.out.println("test:" + coinspec);
+
+		return coinspec;
+
+	}
+
 	// 일반(사양,설비)관리 update
-		@RequestMapping(value="/updateSpec.do", method=RequestMethod.POST)
-		public String Update(BusinessVO vo) {
-			System.out.println("메서드 진입" + vo);
-			userService.updateSpec(vo);
-			if(vo.getBizType() == 1) {
-				return "/jsp/mypageBiz/comspec.jsp";				
-			} 
-			return "/jsp/mypageBizCoin/coinspec.jsp";				
+	@RequestMapping(value = "/updateSpec.do", method = RequestMethod.POST)
+	public String Update(BusinessVO vo) {
+		System.out.println("메서드 진입" + vo);
+		userService.updateSpec(vo);
+		if (vo.getBizType() == 1) {
+			return "/jsp/mypageBiz/comspec.jsp";
 		}
-	    @RequestMapping(value="/findId.do", method=RequestMethod.POST)
-	    public String findId(AccountVO vo, Model model) {
-	        System.out.println("findID 진입");
-	        System.out.println(vo);
-	        model.addAttribute("userId", userService.findId(vo));
-	        return "/jsp/login/findIdConfirmed.jsp";
-	    }
+		return "/jsp/mypageBizCoin/coinspec.jsp";
+	}
+
+	@RequestMapping(value = "/findId.do", method = RequestMethod.POST)
+	public String findId(AccountVO vo, Model model) {
+		System.out.println("findID 진입");
+		System.out.println(vo);
+		model.addAttribute("userId", userService.findId(vo));
+		return "/jsp/login/findIdConfirmed.jsp";
+	}
 }
