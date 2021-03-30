d29232c14fc9e85b9da5ec9a7d59b1e2bba8ac06
sujeongyang <tnwjd7137@gmail.com>
2021-03-30 18:49:30 +0900

    userDAO

--- kkaekkt/src/main/java/com/kkaekkt/biz/user/impl/UserDAO.java
+++ kkaekkt/src/main/java/com/kkaekkt/biz/user/impl/UserDAO.java
@@ -153,14 +153,13 @@ public class UserDAO {
 	
 	// 업체 찜 당한 수
 	public int countLikeBs(BusinessVO vo) {
-//		return  mybatis.selectOne("UserDAO.countLikeBs", vo);
-		return 0;
+		return  mybatis.selectOne("UserDAO.countLikeBs", vo);
 	}
 	// 업체 평균 평점
 	public double avgGradeBs(BusinessVO vo) {
 		return  mybatis.selectOne("UserDAO.avgGradeBs", vo);
 	}
-	// test
+
 
 
 	
