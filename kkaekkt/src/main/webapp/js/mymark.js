c9097abe258f77bb04be55f5bc47b8c6e5a8c765
hty019 <hty018@naver.com>
2021-04-04 12:55:47 +0900

    마이페이지(개인) 찜한 업체 css 완료

--- kkaekkt/src/main/webapp/js/mymark.js
+++ kkaekkt/src/main/webapp/js/mymark.js
@@ -41,29 +41,81 @@ function printList(list) {
     $.each(list,function(key,value){
         console.log(key+'..반복문 돌아가는중');
     $('.content').append(
-        '<div class="card" id="card'+value.bno+'">'+
-            '<div class="info">'+
-                '<p class="bsname" id="bno'+value.bno+'">'+value.bname+'</p>'+
-                '<table>'+
-                    '<tr>'+
-                        '<td><i class="fas fa-star"></i><span> '+value.eval+'</span> | 리뷰 '+value.count+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td class="bsaddress">'+value.address+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td class="bsphone"><i class="fas fa-phone-square-alt"></i>'+value.phone+'</td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td><i class="far fa-heart"></i> 세탁소 찜 <span>123</span></td>'+
-                    '</tr>'+
-                    '<tr>'+
-                        '<td id="kkaekktTag">kkaekkt 가맹점 입니다.</td>'+
-                    '</tr>'+
-                '</table>'+
-            '</div>'+
-            '<button class="btn_mark"><i class="fas fa-heart" value='+value.bno+'></i></button>'+
-            '<button id="rsvBtn">예약하기</button>'+
-        '</div>');
+        // '<div class="card" id="card'+value.bno+'">'+
+        //     '<div class="info">'+
+        //         '<p class="bsname" id="bno'+value.bno+'">'+value.bname+'</p>'+
+        //         '<table>'+
+        //             '<tr>'+
+        //                 '<td><i class="fas fa-star"></i><span> '+value.eval+'</span> | 리뷰 '+value.count+'</td>'+
+        //             '</tr>'+
+        //             '<tr>'+
+        //                 '<td class="bsaddress">'+value.address+'</td>'+
+        //             '</tr>'+
+        //             '<tr>'+
+        //                 '<td class="bsphone"><i class="fas fa-phone-square-alt"></i>'+value.phone+'</td>'+
+        //             '</tr>'+
+        //             '<tr>'+
+        //                 '<td><i class="far fa-heart"></i> 세탁소 찜 <span>123</span></td>'+
+        //             '</tr>'+
+        //             '<tr>'+
+        //                 '<td id="kkaekktTag">kkaekkt 가맹점 입니다.</td>'+
+        //             '</tr>'+
+        //         '</table>'+
+        //     '</div>'+
+        //     '<button class="btn_mark"><i class="fas fa-heart" value='+value.bno+'></i></button>'+
+        //     '<button id="rsvBtn">예약하기</button>'+
+        // '</div>');
+    //     '<div class="card">'+
+    //     '<div class="bsTagLeft">'+
+    //         '<div class="tagTop">'+
+    //             '<div class="like">'+
+    //                 '<i class="fas fa-heart" value='+value.bno+'></i>'+
+    //             '</div>'+
+    //             '<p>'+value.bname+'</p>'+
+    //         '</div>'+
+    //         '<div class="tagBody"> '+
+    //             '<i class="fas fa-map-marker-alt"></i>'+
+    //             '<span>'+value.address+'</span>'+
+    //         '</div>'+
+    //         '<div class="tagBottom">'+
+    //             '<i class="fas fa-phone-alt"></i>'+
+    //             '<span>'+value.phone+'</span>'+
+    //         '</div>'+
+    //     '</div>'+
+    //     '<div class="bsTagRight">'+
+    //         '<div class="bsData">'+
+    //             '<i class="fas fa-star"></i> '+
+    //             '리뷰<span> '+value.count+' </span> | '+
+    //             '<i class="fas fa-heart"></i> '+
+    //             '찜 <span>123</span>'+
+    //         '</div>'+
+    //         '<button>예약하기</button>'+
+    //     '</div>'+
+    // '</div>');
+                '<div class="card">'+
+                '<div class="bsTagLeft">'+
+                    '<div class="tagTop">'+
+                        '<div class="like">'+
+                            '<i class="fas fa-heart like"></i>'+
+                        '</div>'+
+                        '<p>'+value.bname+'</p>'+
+                    '</div>'+
+                    '<div class="tagBottom">'+
+                        '<i class="fas fa-map-marker-alt"></i>'+
+                        '<span class="address">'+value.address+'</span>'+
+                        '<i class="fas fa-phone-alt"></i>'+
+                        '<span class="phone">'+value.phone+'</span>'+
+                        '<i class="fas fa-star"></i>'+
+                        '평점<span class="eval">2.4</span>'+
+                        '<i class="fas fa-user-edit"></i>'+
+                        '리뷰<span class="reviewCount">2,345</span>'+
+                        '<i class="fas fa-heart"></i>'+
+                        '좋아요<span class="likeCount">123</span>'+
+                    '</div>'+
+                '</div>'+
+                '<div class="bsTagRight">'+
+                    '<button>예약하기</button>'+
+                '</div>'+
+            '</div>');
     });
 }
