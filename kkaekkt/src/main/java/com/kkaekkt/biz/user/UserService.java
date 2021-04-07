e68e62054d82b0c92b97f725a4895de401f69ef9
DESKTOP-VB5L3JM\cheun <cheung_co@naver.com>
2021-04-07 22:28:43 +0900

    user 관련 이슈 해결

--- kkaekkt/src/main/java/com/kkaekkt/biz/user/UserService.java
+++ kkaekkt/src/main/java/com/kkaekkt/biz/user/UserService.java
@@ -2,35 +2,36 @@ package com.kkaekkt.biz.user;
 
 import java.util.List;
 
+import com.google.gson.JsonElement;
 import com.kkaekkt.biz.comm.LaundryVO;
 
-
 public interface UserService {
-	public void insertUser(PersonVO vo);
-	public void insertUser(BusinessVO vo);
-	public void updateUser(PersonVO vo);
-	public void updateUser(BusinessVO vo);
-	public void deleteUser(PersonVO vo);
-	public void deleteUser(BusinessVO vo);
-	public void likeOn(BusinessVO vo);
-	public void likeOff(BusinessVO vo);
-	public AccountVO getUser(AccountVO vo);
-	public List<BusinessVO> getLikedBs(int mno);
-	public BusinessVO getComspec(BusinessVO vo);
-	public BusinessVO getCoinspec(BusinessVO vo);
-	public void updateSpec(BusinessVO vo);
-	public AccountVO findId(AccountVO vo);
-	public AccountVO findPw(AccountVO vo);
-	public int idchk(PersonVO vo);
-	public int emailchk(AccountVO vo);
-	public PersonVO method(PersonVO vo);
-	public AccountVO joinCfm(AccountVO vo);
-	public double avgGradeBs(BusinessVO vo);
-	public int countLikeBs(BusinessVO vo);	
-	public int idchkBs(BusinessVO vo);
-	public PersonVO getPerson(int mno);
-	public BusinessVO getBusiness(BusinessVO vo);
-	public List<LaundryVO> getSalse(int bno);
-	public void updatePw(AccountVO vo);
+   public void insertUser(PersonVO vo);
+   public void insertUser(BusinessVO vo);
+   public void updateUser(PersonVO vo);
+   public void updateUser(BusinessVO vo);
+   public String deleteUser(AccountVO vo);
+   public void deleteUser(BusinessVO vo);
+   public void likeOn(BusinessVO vo);
+   public void likeOff(BusinessVO vo);
+   public AccountVO getUser(AccountVO vo);
+   public List<BusinessVO> getLikedBs(int mno);
+   public BusinessVO getComspec(BusinessVO vo);
+   public BusinessVO getCoinspec(BusinessVO vo);
+   public void updateSpec(BusinessVO vo);
+   public AccountVO findId(AccountVO vo);
+   public AccountVO findPw(AccountVO vo);
+   public int idchk(PersonVO vo);
+   public int emailchk(String email);
+   public AccountVO method(AccountVO vo);
+   public AccountVO joinCfm(AccountVO vo);
+   public double avgGradeBs(BusinessVO vo);
+   public int countLikeBs(BusinessVO vo);   
 
+   public PersonVO getPerson(int mno);
+   public BusinessVO getBusiness(BusinessVO vo);
+   public List<LaundryVO> getLaundryList(int bno);
+   public void updatePw(AccountVO vo);
+   public int bnoChk(int bno);
+   
 }
